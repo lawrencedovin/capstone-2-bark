1be8321e3e9614c1c6c34a0e73d1e938556a6cd2
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-08 14:44:44 -0400

    Installed gen react cli and added navbar component

---
+++ bark/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const NavBar = () => (
+  <div data-testid="NavBar">
+    NavBar Component
+  </div>
+);
+
+NavBar.propTypes = {};
+
+NavBar.defaultProps = {};
+
+export default NavBar;
