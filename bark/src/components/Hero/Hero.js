d69b208801678b3a451e7664df04cbacc708dd2f
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-11 15:00:59 -0400

    Added social icons

--- bark/src/components/Hero/Hero.js
+++ bark/src/components/Hero/Hero.js
@@ -7,16 +7,22 @@ const Hero = () => (
       <div className="row text-center align-items-center">
         <div className="col-md-1">
           <ul className="hero__logo">
-            <li className="hero__logo-item">1</li>
-            <li className="hero__logo-item">2</li>
-            <li className="hero__logo-item">3</li>
+            <li className="hero__logo-item">
+              <img src={process.env.PUBLIC_URL + 'icons/linkedin-icon.svg'} className="" alt="linkedin"/>
+            </li>
+            <li className="hero__logo-item">
+            <img src={process.env.PUBLIC_URL + 'icons/github-icon.svg'} className="" alt="github"/>
+            </li>
+            <li className="hero__logo-item">
+            <img src={process.env.PUBLIC_URL + 'icons/email-icon.svg'} className="" alt="email"/>
+            </li>
           </ul>
         </div>
         <div className="col-md-10">
           <img src={process.env.PUBLIC_URL + 'images/hero-image.jpg'} className="hero__image img-fluid" alt="hero"/> 
         </div>
         <div className="col-md-1">
-          hello
+          Scroll
         </div>
       </div>
     </div>
