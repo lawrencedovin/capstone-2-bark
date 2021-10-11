051ed00cc37c775f519fa5500b2f6bd3655841df
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-11 14:54:48 -0400

    Added initial Hero items

--- bark/src/components/Hero/Hero.js
+++ bark/src/components/Hero/Hero.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 const Hero = () => (
   <div data-testid="Hero">
-    Hero Component
+    <div className="hero container-fluid">
+      <div className="row text-center align-items-center">
+        <div className="col-md-1">
+          <ul className="hero__logo">
+            <li className="hero__logo-item">1</li>
+            <li className="hero__logo-item">2</li>
+            <li className="hero__logo-item">3</li>
+          </ul>
+        </div>
+        <div className="col-md-10">
+          <img src={process.env.PUBLIC_URL + 'images/hero-image.jpg'} className="hero__image img-fluid" alt="hero"/> 
+        </div>
+        <div className="col-md-1">
+          hello
+        </div>
+      </div>
+    </div>
   </div>
 );
 
