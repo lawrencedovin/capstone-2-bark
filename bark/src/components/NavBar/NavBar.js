2745e57a58ee653ea615b658caee6fd250d5921b
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-11 09:27:38 -0400

    Updated navbar styles

--- bark/src/components/NavBar/NavBar.js
+++ bark/src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => (
   <div data-testid="NavBar">
     <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
-        <a class="navbar-brand navbar__logo" href="#">Bark</a>
+        <a class="navbar__logo" href="/">Bark</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -14,13 +14,13 @@ const NavBar = () => (
           <div class="d-flex">
             <ul class="navbar-nav me-2">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link navbar__link" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link navbar__link" href="/">Link</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <a class="nav-link navbar__link" aria-current="page" href="/">Home</a>
               </li>
             </ul>
           </div>
