9151efe20ec7a7310dea29b6f6273a0c71ab65fe
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-13 16:08:05 -0400

    Added like icon

--- bark/src/components/LoggedInCards/LoggedInCards.js
+++ bark/src/components/LoggedInCards/LoggedInCards.js
@@ -5,6 +5,9 @@ const LoggedInCards = ({imgUrl, title, description, btnText}) => (
   <div data-testid="LoggedInCards">
     <div class="card logged-in-card">
       <img class="card-img-top" src={imgUrl} alt={title} />
+      <div className="logged-in-card__like">
+          icon
+      </div>
       <div class="card-body logged-in-card__body">
         <h5 class="card-title logged-in-card__title">{title}</h5>
         <p class="card-text logged-in-card__description">{description}</p>
