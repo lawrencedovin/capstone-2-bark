9c16ee3e8214ed34a5c05d6abc085296326be566
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-13 14:30:48 -0400

    Updated home separator styles

--- bark/src/components/HomeSeperator/HomeSeperator.js
+++ bark/src/components/HomeSeperator/HomeSeperator.js
@@ -3,11 +3,15 @@ import React from 'react';
 
 const HomeSeperator = ({title}) => (
   <div data-testid="HomeSeperator">
-    <div className="home-seperator">
-      <h1 className="home-separator__title">
-        {title}
-      </h1>
-      <div className="home-seperator__underline"></div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="home-seperator col-md-10 mx-auto">
+          <h1 className="home-separator__title">
+            {title}
+          </h1>
+          <div className="home-seperator__underline"></div>
+        </div>
+      </div>
     </div>
   </div>
 );
