6ae299dee42a96eacddad286fc577c76f366551f
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-13 13:56:54 -0400

    Updated App

--- bark/src/App.js
+++ bark/src/App.js
@@ -1,13 +1,9 @@
-import NavBar from './components/NavBar/NavBar';
-import Hero from './components/Hero/Hero';
-import DogSearchFilter from './components/DogSearchFilter/DogSearchFilter';
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Hero />
-      <DogSearchFilter />
+      <Home />
     </div>
   );
 }
