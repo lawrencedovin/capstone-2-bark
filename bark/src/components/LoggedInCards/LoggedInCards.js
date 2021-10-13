37237e9e05686f7559a71b8279fe0f5f0dcbaeb4
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-13 15:51:51 -0400

    Updated card button

--- bark/src/components/LoggedInCards/LoggedInCards.js
+++ bark/src/components/LoggedInCards/LoggedInCards.js
@@ -5,12 +5,12 @@ const LoggedInCards = ({imgUrl, title, description, btnText}) => (
   <div data-testid="LoggedInCards">
     <div class="card logged-in-card">
       <img class="card-img-top" src={imgUrl} alt={title} />
-      <div class="card-body">
+      <div class="card-body logged-in-card__body">
         <h5 class="card-title logged-in-card__title">{title}</h5>
         <p class="card-text logged-in-card__description">{description}</p>
-        <a href="/" class="btn btn-primary logged-in-card__button">{btnText}</a>
       </div>
     </div>
+    <a href="/" class="btn logged-in-card__button">{btnText}</a>
   </div>
 );
 
