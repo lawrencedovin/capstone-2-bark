db47432c536dbc311eeabc29c5b2d6de9f012b54
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-13 09:28:08 -0400

    Updated select dropdown menu

--- bark/src/components/DogSearchFilter/DogSearchFilter.js
+++ bark/src/components/DogSearchFilter/DogSearchFilter.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
+function DogSearchFilter() {
 
-const DogSearchFilter = () => (
+  const [breed, setBreed] = useState('All Breeds');
+
+  const handleChange = e => {
+    alert(e.target.value);
+    setBreed(e.target.value);
+  };
+
+  return (
   <div data-testid="DogSearchFilter">
     <div className="row">
       <div className="col-md-12">
         <form className="dog-search-filter text-center">
           <span class="dog-search-filter__label">Find Best Friend: </span>
           <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <div class="btn-group" role="group">
-              <button id="dogSearchFilter" type="button" class="btn dropdown-toggle dog-search-filter__button dog-search-filter__button--dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                All Breeds
+            <select name="breeds" class="dog-search-filter__button dog-search-filter__button--dropdown">
+              <option value="all breeds">All Breeds</option>
+              <option value="husky">Husky</option>
+              <option value="english bulldog">English Bulldog</option>
+            </select>
+            {/* <div class="btn-group" role="group">
+              <button id="dogSearchFilter" type="button" onChange={handleChange} class="btn dropdown-toggle dog-search-filter__button dog-search-filter__button--dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                {breed}
               </button>
               <ul class="dropdown-menu" aria-labelledby="dogSearchFilter">
-                <li><a class="dropdown-item dog-search-filter__button" href="/">Husky</a></li>
-                <li><a class="dropdown-item dog-search-filter__button" href="/">English Bulldog</a></li>
+                <li><button class="dropdown-item dog-search-filter__button" type="button" value={"All Breeds"}>All Breeds</button></li>
+                <li><button class="dropdown-item dog-search-filter__button" type="button" value={"Husky"}>Husky</button></li>
+                <li><button class="dropdown-item dog-search-filter__button" type="button" value={"English Bulldog"}>English Bulldog</button></li>
               </ul>
-            </div>
+            </div> */}
             <div class="input-group">
               <input type="text" class="form-control dog-search-filter__input" placeholder="Zip Code" aria-label="Input group example" aria-describedby="btnGroupAddon" />
             </div>
@@ -29,7 +43,8 @@ const DogSearchFilter = () => (
       </div>
     </div>
   </div>
-);
+  );
+}
 
 DogSearchFilter.propTypes = {};
 
