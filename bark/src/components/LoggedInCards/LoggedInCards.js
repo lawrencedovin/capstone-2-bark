2368bf70827cc20f40cabcbc46187b5f300ce654
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-14 16:02:15 -0400

    Updated icon so that image changes on hover

--- bark/src/components/LoggedInCards/LoggedInCards.js
+++ bark/src/components/LoggedInCards/LoggedInCards.js
@@ -5,8 +5,8 @@ const LoggedInCards = ({imgUrl, title, description, btnText}) => (
   <div data-testid="LoggedInCards">
     <div class="card logged-in-card">
       <img class="card-img-top" src={imgUrl} alt={title} />
-      <div className="logged-in-card__like">
-          icon
+      <div className="logged-in-card__like d-flex align-items-center justify-content-center">
+        <div className="logged-in-card__like-icon img-fluid" alt="like"></div>
       </div>
       <div class="card-body logged-in-card__body">
         <h5 class="card-title logged-in-card__title">{title}</h5>
