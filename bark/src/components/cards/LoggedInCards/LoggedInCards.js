27711c611ba8e4ce582ebddad5544d1f909f01a7
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-15 15:34:50 -0400

    Added login status for cards

--- bark/src/components/cards/LoggedInCards/LoggedInCards.js
+++ bark/src/components/cards/LoggedInCards/LoggedInCards.js
@@ -1,29 +1,37 @@
 import React from 'react';
 
-const LoggedInCards = ({imgUrl, title, description, btnText}) => (
-  <div data-testid="LoggedInCards">
-    <div className="logged-in-card">
-      <div className="logged-in-card__img-body-container">
-        <img class="card-img-top" src={imgUrl} alt={title} />
-        <button className="logged-in-card__like d-flex align-items-center justify-content-center">
-            <div className="logged-in-card__like-icon img-fluid" alt="like"></div>
-        </button>
-        <div class="card-body logged-in-card__body">
-          <h5 class="card-title logged-in-card__title">{title}</h5>
-          {description 
-          ? 
-          <>
-            <p class="card-text logged-in-card__description">Breed: {description.breed}</p>
-            <p class="card-text logged-in-card__description">Location: {description.location}</p>
-          </>
+function LoggedInCards({imgUrl, title, description, btnText}) {
+  let isLoggedIn = false;
+  return(
+    <div data-testid="LoggedInCards">
+      <div className="logged-in-card">
+        <div className="logged-in-card__img-body-container">
+          <img class="card-img-top" src={imgUrl} alt={title} />
+          {isLoggedIn
+          ?
+          <button className="logged-in-card__like d-flex align-items-center justify-content-center">
+              <div className="logged-in-card__like-icon img-fluid" alt="like"></div>
+          </button>
           :
           <></>
           }
+          <div class="card-body logged-in-card__body">
+            <h5 class="card-title logged-in-card__title">{title}</h5>
+            {description 
+            ? 
+            <>
+              <p class="card-text logged-in-card__description">Breed: {description.breed}</p>
+              <p class="card-text logged-in-card__description">Location: {description.location}</p>
+            </>
+            :
+            <></>
+            }
+          </div>
         </div>
+        <a href="/" class="btn logged-in-card__button">{btnText}</a>
       </div>
-      <a href="/" class="btn logged-in-card__button">{btnText}</a>
     </div>
-  </div>
-);
+  );
+}
 
 export default LoggedInCards;
