438fd2b8fc9cf08187233069e1cfca6780d044ba
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-15 08:26:14 -0400

    Updated like icon on card

--- bark/src/components/LoggedInCards/LoggedInCards.js
+++ bark/src/components/LoggedInCards/LoggedInCards.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 const LoggedInCards = ({imgUrl, title, description, btnText}) => (
   <div data-testid="LoggedInCards">
+    <div className="logged-in-card__like d-flex align-items-center justify-content-center">
+        <div className="logged-in-card__like-icon img-fluid" alt="like"></div>
+    </div>
     <div class="card logged-in-card">
       <img class="card-img-top" src={imgUrl} alt={title} />
-      <div className="logged-in-card__like d-flex align-items-center justify-content-center">
-        <div className="logged-in-card__like-icon img-fluid" alt="like"></div>
-      </div>
       <div class="card-body logged-in-card__body">
         <h5 class="card-title logged-in-card__title">{title}</h5>
         <p class="card-text logged-in-card__description">{description}</p>
