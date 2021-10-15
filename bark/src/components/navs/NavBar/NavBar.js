76fb8968b59a47f4c39a995e07ddba0d2c2d6745
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-15 15:32:12 -0400

    added is logged in status to nav bar

--- bark/src/components/navs/NavBar/NavBar.js
+++ bark/src/components/navs/NavBar/NavBar.js
@@ -1,35 +1,50 @@
 import React from 'react';
 
-const NavBar = () => (
-  <div data-testid="NavBar">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <a class="navbar__logo" href="/">Bark</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <div class="me-auto"></div>
-          <div class="d-flex">
-            <ul class="navbar-nav me-2">
-              <li class="nav-item">
-                <a class="nav-link navbar__link" href="/">lawrence123</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link navbar__link" href="/">Breeds</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link navbar__link" href="/">Dogs</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link navbar__link" href="/">Logout</a>
-              </li>
-            </ul>
+function NavBar() {
+  let isLoggedIn = false;
+  return(
+    <div data-testid="NavBar">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar__logo" href="/">Bark</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div class="me-auto"></div>
+            <div class="d-flex">
+              {isLoggedIn
+              ?
+                <ul class="navbar-nav me-2">
+                  <li class="nav-item">
+                    <a class="nav-link navbar__link" href="/">lawrence123</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link navbar__link" href="/">Breeds</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link navbar__link" href="/">Dogs</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link navbar__link" href="/">Logout</a>
+                  </li>
+                </ul>
+              :
+                <ul class="navbar-nav me-2">
+                    <li class="nav-item">
+                      <a class="nav-link navbar__link" href="/">Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link navbar__link" href="/">Login</a>
+                    </li>
+                </ul>
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  </div>
-);
+      </nav>
+    </div>
+  );
+}
 
 export default NavBar;
