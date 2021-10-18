ba0dbb9bdcf177299455d916c270bb6ed4fc5c5a
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-18 15:59:21 -0400

    Updated edit profile form

--- bark/src/main-pages/EditUser/EditUser.js
+++ bark/src/main-pages/EditUser/EditUser.js
@@ -7,8 +7,8 @@ function EditUser() {
         <div className="row">
             <div className="col-md-12 forms__container">
               <form className="forms col-md-4">
-                <h1 className="forms__title">Login</h1>
-                <p className="forms__description">Enter your user credentials to login</p>
+                <h1 className="forms__title">Edit Profile</h1>
+                <p className="forms__description">Update your user credentials and save changes</p>
                 <div class="input-group forms__input-container align-items-center">
                   <input type="text" placeholder="Username" className="form-control forms__input" />
                   <span class="input-group-btn forms__input-icon align-middle">
@@ -16,13 +16,20 @@ function EditUser() {
                   </span>
                 </div>
                 <div class="input-group forms__input-container align-items-center">
+                  <input type="text" placeholder="Email" className="form-control forms__input" />
+                  <span class="input-group-btn forms__input-icon align-middle">
+                      <i class="fas fa-envelope"></i>
+                  </span>
+                </div>
+                <p>To confirm changes, enter your password</p>
+                <div class="input-group forms__input-container align-items-center">
                   <input type="password" placeholder="Password" className="form-control forms__input" />
                   <span class="input-group-btn forms__input-icon align-middle">
                       <i class="fas fa-lock"></i>
                   </span>
                 </div>
                 <button href="#" class="forms__button d-flex justify-content-between align-items-center">
-                  Login
+                  Save Changes
                   <img src={process.env.PUBLIC_URL + 'icons/button-arrow.svg'} alt="Form Button" class="forms__button-icon" />
                 </button>
               </form>
