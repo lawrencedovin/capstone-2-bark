014f0bccac3ae235d63dd6cd968b978adcdf0277
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-18 16:11:22 -0400

    Added initial search page

---
+++ bark/src/main-pages/Search/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import DogSearchFilter from '../../components/filters/DogSearchFilter/DogSearchFilter';
+import HomeSeperator from '../../components/titles/HomeSeperator/HomeSeperator';
+import BreedsCardsList from '../../components/cards/BreedsCardsList/BreedsCardsList';
+import DogsCardsList from '../../components/cards/DogsCardsList/DogsCardsList';
+import MainFooter from '../../components/footers/MainFooter/MainFooter';
+
+
+function Search() {
+  return(
+    <div data-testid="Search">
+      Search Component
+    </div>
+  );
+}
+
+export default Search;
