bf70b0fa5870cdae5060db6cd2175304926914bf
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-18 15:33:06 -0400

    Added edit user route

--- bark/src/App.js
+++ bark/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./main-pages/Home/Home";
 import Register from "./main-pages/Register/Register"
 import Login from "./main-pages/Login/Login";
+import EditUser from "./main-pages/EditUser/EditUser";
 import NavBar from "./components/navs/NavBar/NavBar";
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route exact path="/login">
           <Login />
+        </Route>
+        <Route exact path="/user/edit">
+          <EditUser />
         </Route>
       </BrowserRouter>
     </div>
