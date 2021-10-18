a55c3ce5b8e81590f118f1d5b7285e939cb37ce5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-18 09:07:11 -0400

    Added initial form inputs

--- bark/src/main-pages/Register/Register.js
+++ bark/src/main-pages/Register/Register.js
@@ -14,7 +14,26 @@ function Register() {
               <form className="register__form col-md-8">
                 <h1 className="register__form-title">Create an Account</h1>
                 <p className="register__form-description">Please fill out the form to create an account</p>
-                <div class="mb-3">
+                
+                <div class="input-group register__form-input-container align-items-center">
+                  <input type="text" placeholder="Username" className="form-control register__form-input" />
+                  <span class="input-group-btn register__form-input-icon align-middle">
+                      <i class="fas fa-user"></i>
+                  </span>
+                </div>
+                <div class="input-group register__form-input-container align-items-center">
+                  <input type="text" placeholder="Email" className="form-control register__form-input" />
+                  <span class="input-group-btn register__form-input-icon align-middle">
+                      <i class="fas fa-envelope"></i>
+                  </span>
+                </div>
+                <div class="input-group register__form-input-container align-items-center">
+                  <input type="password" placeholder="Password" className="form-control register__form-input" />
+                  <span class="input-group-btn register__form-input-icon align-middle">
+                      <i class="fas fa-lock"></i>
+                  </span>
+                </div>
+                {/* <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
                   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                   <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
@@ -26,7 +45,7 @@ function Register() {
                 <div class="mb-3 form-check">
                   <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
+                </div> */}
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
               
