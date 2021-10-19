6b040454e415a70bdef0dc6dbe3d1db2979c9d62
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-19 17:03:27 -0400

    Updated details image

--- bark/src/main-pages/BreedDetails/BreedDetails.js
+++ bark/src/main-pages/BreedDetails/BreedDetails.js
@@ -5,18 +5,16 @@ const BreedDetails = () => (
   <div data-testid="BreedDetails">
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-7 mx-auto">
-          <div className="row justify-content-center text-center">
-            <div className="details">
-              <div className="details__title-icon-container">
-                <button className="details__like d-flex align-items-center justify-content-center">
-                  <div className="details__like-icon img-fluid" alt="like"></div>
-                </button>
-                <h1 className="details__title">Akita</h1>
-                <div className="col-md-4">
-                  <img src={process.env.PUBLIC_URL + 'images/akita.jpg'} className="details__image img-fluid" alt="akita"/>
-                </div>
-              </div>
+        <div className="col-md-7 mx-auto details">
+            <div className="details__title-icon-container">
+              <button className="details__like">
+                <div className="details__like-icon" alt="like"></div>
+              </button>
+              <h1 className="details__title">Akita</h1>
+            </div>
+          <div className="row">
+            <div className="col-md-4">
+                <img src={process.env.PUBLIC_URL + 'images/akita.jpg'} className="details__image img-fluid" alt="akita"/>
             </div>
           </div>
         </div>
