0965e4537df087c9717ec32bc72b54e7c4205e05
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-21 15:58:48 -0400

    Updated favorite breeds list for user

--- bark/routes/users.js
+++ bark/routes/users.js
@@ -71,4 +71,26 @@ router.get('/:id/liked_dogs', async (req, res, next) => {
     }
 });
 
+router.get('/:id/favorite_breeds', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const results = await db.query(
+            `SELECT u.username as username,
+            b.breeds as breeds
+            FROM users u
+            JOIN favorite_breeds b
+                ON b.user_id = u.id 
+            WHERE u.id=$1`,
+            [id]
+        );
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find user with id of ${id}`, 404);
+        }
+        return res.json({user: results.rows[0]});
+    }
+    catch(e) {
+        return next(e);
+    }
+});
+
 module.exports = router;
