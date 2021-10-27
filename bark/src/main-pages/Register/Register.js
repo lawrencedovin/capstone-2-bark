235e222ad389955e6915e11d4338aee35dec7678
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-27 18:16:54 -0400

    Refactored code

--- bark/src/main-pages/Register/Register.js
+++ bark/src/main-pages/Register/Register.js
@@ -18,10 +18,6 @@ function Register() {
   }
 
   const [formData, setFormData] = useState(FORM_INPUT_INITIAL_STATE);
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertContent, setAlertContent] = useState('');
-  const [alertType, setAlertType] = useState('');
-
   const [alertData, setAlertData] = useState(ALERT_INITIAL_STATE);
 
   let [disabledButton, setDisableButton] = useState(true);
@@ -65,9 +61,7 @@ function Register() {
     return response.json(); // parses JSON response into native JavaScript objects
   }
 
-  // const resetShowAlert = () => setShowAlert(false);
   const resetShowAlert = () => setAlertData(ALERT_INITIAL_STATE);
-  // Reset set AlertType
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -76,13 +70,11 @@ function Register() {
     .then(data => {
       setAlertData(alertData => ({...alertData, 'visible': true}));
       if(data.hasOwnProperty('user')) {
-        setAlertContent(`${data.user.username} successfully created account!`);
+        setAlertData(alertData => ({...alertData, 'content': `${data.user.username} successfully created account!`}));
       }
       else {
-        setAlertType('error');
-        setAlertContent(`${data.error.message}`);
+        setAlertData(alertData => ({...alertData, 'type': 'error', 'content': `${data.error.message}`}));
       }
-      alert(data); // JSON data parsed by `data.json()` call
     });
     setFormData(FORM_INPUT_INITIAL_STATE);
   }
@@ -103,17 +95,17 @@ function Register() {
               <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </symbol>
           </svg>
-          {alertType === 'error'
+          {alertData.type === 'error'
           ?
           <div class="alert alert-warning d-flex align-items-center alert-dismissible fade show" role="alert">
             <svg class="bi flex-shrink-0 me-4" width="24" height="24" role="img" aria-label="Warning:"><use xlinkHref="#exclamation-triangle-fill"/></svg>
-            <p className="alert__content">{alertContent}</p>
+            <p className="alert__content">{alertData.content}</p>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={resetShowAlert}></button>
           </div>
           :
           <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
             <svg class="bi flex-shrink-0 me-4" width="24" height="24" role="img" aria-label="Success:"><use xlinkHref="#check-circle-fill"/></svg>
-            <p className="alert__content">{alertContent}</p>
+            <p className="alert__content">{alertData.content}</p>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={resetShowAlert}></button>
           </div>
           }
