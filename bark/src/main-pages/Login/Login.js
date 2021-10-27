8b1a5daa9d8b425e1efa88da1d153d7d6ef4b368
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-27 08:36:22 -0400

    Updated login form to use UserInput component

--- bark/src/main-pages/Login/Login.js
+++ bark/src/main-pages/Login/Login.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormFooter from '../../components/footers/FormFooter/FormFooter';
+import UserInput from '../../components/forms/UserInput/UserInput';
 
 function Login() {
+
+  const FORM_INPUT_INITIAL_STATE = {
+    username: '',
+    password: ''
+  };
+
+  const [formData, setFormData] = useState(FORM_INPUT_INITIAL_STATE);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(formData => ({
+      ...formData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = ((e) => {
+    e.preventDefault();
+    const { username, password } = formData;
+    alert(`User: ${username}-${password}`);
+    setFormData(FORM_INPUT_INITIAL_STATE);
+  })
+
   return(
     <div data-testid="Login">
         <div className="row">
@@ -9,17 +33,29 @@ function Login() {
               <img src={process.env.PUBLIC_URL + 'images/forms/login-form-photo.jpg'} alt="register dog" className="forms__image" />
             </div>
             <div className="col forms__container">
-              <form className="forms col-md-8">
+              <form className="forms col-md-8" onSubmit={handleSubmit}>
                 <h1 className="forms__title">Login</h1>
                 <p className="forms__description">Enter your user credentials to login.</p>
                 <div class="input-group forms__input-container align-items-center">
-                  <input type="text" placeholder="Username" className="form-control forms__input" />
-                  <span class="input-group-btn forms__input-icon align-middle">
-                      <i class="fas fa-user"></i>
-                  </span>
+                  <UserInput 
+                        name="username" 
+                        value={formData.username} 
+                        type="text"
+                        handleChange={handleChange}
+                        className="form-control forms__input"
+                    />
+                    <span class="input-group-btn forms__input-icon align-middle">
+                        <i class="fas fa-user"></i>
+                    </span>
                 </div>
                 <div class="input-group forms__input-container align-items-center">
-                  <input type="password" placeholder="Password" className="form-control forms__input" />
+                  <UserInput 
+                        name="password" 
+                        value={formData.password} 
+                        type="password"
+                        handleChange={handleChange}
+                        className="form-control forms__input"
+                  />
                   <span class="input-group-btn forms__input-icon align-middle">
                       <i class="fas fa-lock"></i>
                   </span>
