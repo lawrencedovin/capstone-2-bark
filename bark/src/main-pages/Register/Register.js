cbd714a97771c9cceff1a7fc48e8ba0ce923e634
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-27 18:11:16 -0400

    Added alertdata initial state

--- bark/src/main-pages/Register/Register.js
+++ bark/src/main-pages/Register/Register.js
@@ -11,9 +11,18 @@ function Register() {
     password: ''
   };
 
+  const ALERT_INITIAL_STATE = {
+    visible: false,
+    content: '',
+    type: ''
+  }
+
   const [formData, setFormData] = useState(FORM_INPUT_INITIAL_STATE);
   const [showAlert, setShowAlert] = useState(false);
   const [alertContent, setAlertContent] = useState('');
+  const [alertType, setAlertType] = useState('');
+
+  const [alertData, setAlertData] = useState(ALERT_INITIAL_STATE);
 
   let [disabledButton, setDisableButton] = useState(true);
   let [errorStylesDisplay, setErrorStylesDisplay] = useState(false);
@@ -56,19 +65,21 @@ function Register() {
     return response.json(); // parses JSON response into native JavaScript objects
   }
 
-  const resetShowAlert = () => setShowAlert(false);
+  // const resetShowAlert = () => setShowAlert(false);
+  const resetShowAlert = () => setAlertData(ALERT_INITIAL_STATE);
+  // Reset set AlertType
 
   const handleSubmit = e => {
     e.preventDefault();
     const { username, email, zipcode, password } = formData;
     postData('/users', { username, email, zipcode, password })
     .then(data => {
-      // console.log(data.json)
-      setShowAlert(true);
+      setAlertData(alertData => ({...alertData, 'visible': true}));
       if(data.hasOwnProperty('user')) {
         setAlertContent(`${data.user.username} successfully created account!`);
       }
       else {
+        setAlertType('error');
         setAlertContent(`${data.error.message}`);
       }
       alert(data); // JSON data parsed by `data.json()` call
@@ -78,7 +89,7 @@ function Register() {
 
   return(
     <div data-testid="Register">
-        {showAlert 
+        {alertData.visible 
         ?
         <>
           <svg xmlnsXlink="http://www.w3.org/2000/svg" style={{display: 'none'}}>
@@ -92,11 +103,20 @@ function Register() {
               <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </symbol>
           </svg>
+          {alertType === 'error'
+          ?
+          <div class="alert alert-warning d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg class="bi flex-shrink-0 me-4" width="24" height="24" role="img" aria-label="Warning:"><use xlinkHref="#exclamation-triangle-fill"/></svg>
+            <p className="alert__content">{alertContent}</p>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={resetShowAlert}></button>
+          </div>
+          :
           <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
             <svg class="bi flex-shrink-0 me-4" width="24" height="24" role="img" aria-label="Success:"><use xlinkHref="#check-circle-fill"/></svg>
             <p className="alert__content">{alertContent}</p>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={resetShowAlert}></button>
           </div>
+          }
         </>
         :
         <></>
