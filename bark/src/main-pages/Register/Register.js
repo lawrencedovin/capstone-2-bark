37fd5a23ab59d59e63c4594f2149cc8a35b89ef3
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-29 09:25:56 -0400

    Refactored post data to api helpers

--- bark/src/main-pages/Register/Register.js
+++ bark/src/main-pages/Register/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import FormFooter from '../../components/footers/FormFooter/FormFooter';
 import UserInput from '../../components/forms/UserInput/UserInput';
 import { countdown } from '../../helpers/general-helpers';
+import { postData } from '../../helpers/api-helpers';
 
 function Register() {
 
@@ -41,25 +42,6 @@ function Register() {
       ...formData,
       [name]: value
     }))
-  }
-
-  // Example POST method implementation:
-  async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
   }
 
   const resetShowAlert = () => setAlertData(ALERT_INITIAL_STATE);
