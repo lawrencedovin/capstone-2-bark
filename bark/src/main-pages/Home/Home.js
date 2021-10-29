04c0ac1924967d04902dd5a03e8db1c3c2aaedff
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-29 09:46:12 -0400

    Got access token hoorayyy!

--- bark/src/main-pages/Home/Home.js
+++ bark/src/main-pages/Home/Home.js
@@ -5,11 +5,12 @@ import BreedsCardsList from '../../components/cards/BreedsCardsList/BreedsCardsL
 import DogsCardsList from '../../components/cards/DogsCardsList/DogsCardsList';
 import MainFooter from '../../components/footers/MainFooter/MainFooter';
 import React, {useEffect, useState} from 'react';
+import { postData } from '../../helpers/api-helpers';
 
 function Home() {
   const [state, setState] = useState({data: []});
 
-  let yourAccessTokenHere = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoc1pjOGxSME9vbmxUb0JWTm9tV2RpVXIybm9vZFdIZnAzSGlLclg0dFRoV014SjFjRyIsImp0aSI6ImE5YzZhMDdmMzA4ZjJlZTI0Zjc4ZWE3ZDM5MjE2OTc3MDA5YmRjZjVjZGZjMzNhYzczNTM2MzEzZDkwN2FlYzQyZGM0YjI3ZWM5MGM3YzMwIiwiaWF0IjoxNjM1NTExMDQ3LCJuYmYiOjE2MzU1MTEwNDcsImV4cCI6MTYzNTUxNDY0Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.Sh5KRg7B2jof0HAQVNjE13yPd7SWtqUNS1Zbqx1K4o3e3Z5bhUSDJhnbAxkCkd5EFbCsRDo0o3UrRcHxsOzM-_bvdtsFK0jh8fCOEvtOJzZTl8f5TkS12v0PlkPpGhlH2R1uzxp6KvZPrxwKovl1VQB07hFILqGCqrU6pHOQ8LvSgrrSgtY7aVfKJuxN9IX5Ss9TLig4wP88efQYdPGpzFGxK0RCxDmBcjB3d0Ckl-tmeNLo0uwURoOJ2MkwgfZxcIOZSvtpqST1--mwbwG8DP7swbULSNtSpH11RqBmrPdJFSWkR7pXLZALiNUX_U0D1rhvs6dfKwd4HISO7raFIQ";
+  let yourAccessTokenHere = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoc1pjOGxSME9vbmxUb0JWTm9tV2RpVXIybm9vZFdIZnAzSGlLclg0dFRoV014SjFjRyIsImp0aSI6IjJmY2I1MGJkMjhhNWM2Y2JmZjVmM2U1MmQ5MmVlNGU1NGRkNmY2NDAyYzNjYTMwYTE1MWEzMmEyZWE3YzEwNDhlMWI2ZWE5MTkwOTM4ZDI2IiwiaWF0IjoxNjM1NTE1MTAzLCJuYmYiOjE2MzU1MTUxMDMsImV4cCI6MTYzNTUxODcwMywic3ViIjoiIiwic2NvcGVzIjpbXX0.HUvz_RrfEaORcBM8U4yCDqQulb4BC6kZZpe1DLNtJxShW8Gyl-3x-5MrdcROl3EC3NHQ6GwxUNOHym_Gz_woJvU40mABD3_fYSWOwOz2zP2ZhuhQHqooHGVg6ZTP_k1BIU8sbZa6vBu4mw0y6pGmC-kwcKLQKv7jzI_mf0eqtakFV-ufu3lhE_qeGCILtOoc0vAqsvOZ2JKEwcf_X6gRrGDy4EHHU_5KEwenZ03BCg4qvkJL01OyJu0ohktDnBp_lbL-1HqDExCIImoufnpifO0beTUyd_vnDewt26LBtbAaLKLeLHyvDuhgdt44AK7BUX8f4jVflSysU0Qg9uAzJQ";
 
   // fetching the GET route from the Express server which matches the GET route from server.js
   const callBackendAPI = async () => {
@@ -26,10 +27,20 @@ function Home() {
 
   useEffect(() => {
     console.log('component useEffect');
+    let grant_type = "client_credentials";
+    let client_id = "hsZc8lR0OonlToBVNomWdiUr2noodWHfp3HiKrX4tThWMxJ1cG";
+    let client_secret = "cTGO1vj4Bn9Zvz37zg3D8nJ5kyM5JSEhnR0UqO9M";
+    
+      postData('https://api.petfinder.com/v2/oauth2/token', { grant_type, client_id, client_secret})
+      .then(data => {
+        alert(JSON.stringify(data.access_token));
+    });
+
     callBackendAPI()
       .then(res => setState({ data: res.breeds }))
       .catch(err => console.log(err));
   }, [])
+
 
   // componentDidMount();
 
