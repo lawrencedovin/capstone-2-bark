675553a888344727c4a4a6541e1fab559eee94d2
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-29 16:42:00 -0400

    api call to get data for dogs

--- bark/src/main-pages/Home/Home.js
+++ bark/src/main-pages/Home/Home.js
@@ -10,26 +10,52 @@ import { postData, getData, grant_type, client_id, client_secret } from '../../h
 function Home() {
   const [state, setState] = useState({data: []});
 
+  // useEffect(() => {
+  //   let accessToken;
+    
+  //   postData('https://api.petfinder.com/v2/oauth2/token', { grant_type, client_id, client_secret})
+  //     .then(data => {
+  //       accessToken = data.access_token;
+  //       getData(accessToken, 'https://api.petfinder.com/v2/types/dog/breeds')
+  //       .then(res => {
+  //         setState({ data: res.breeds })
+  //       })
+  //       .catch(err => console.log(err));
+  //   });
+  // }, [])
+
   useEffect(() => {
     let accessToken;
     
     postData('https://api.petfinder.com/v2/oauth2/token', { grant_type, client_id, client_secret})
       .then(data => {
         accessToken = data.access_token;
-        getData(accessToken, 'https://api.petfinder.com/v2/types/dog/breeds')
+        getData(accessToken, 'https://api.petfinder.com/v2/animals?location=20720&type=dog&breed=Pug&limit=4')
         .then(res => {
-          setState({ data: res.breeds })
+          setState({ data: res.animals })
         })
         .catch(err => console.log(err));
     });
   }, [])
+
+  ;
 
   return (
     <div className="Home">
       <Hero />
       <DogSearchFilter />
       <HomeSeperator title="Breeds" />
-      {state.data.map(user => <div>{user.name}</div>)}
+      {/* {state.data.map(user => <div>{user.name}</div>)} */}
+      {state.data.map(dog => 
+      <ul>
+        <li>ID: {dog.id}</li>
+        <li>NAME: {dog.name}</li>
+        <li><img src={dog.photos.medium} alt={dog.name} /></li>
+        <li>Status URL: {dog.status}</li>
+        <li>Location: {dog.contact.address.city}, {dog.contact.address.state}</li>
+      </ul>
+      )}
+      
       <BreedsCardsList/>
       <HomeSeperator title="Dogs" />
       <DogsCardsList/>
