e5576e37ced04efbc3cd13db392f9b0b2a683007
Lawrence Dovin <lawrencedovin@gmail.com>
2021-10-31 15:38:55 -0400

    Udpated dog list breeds

--- bark/src/main-pages/DogList/DogList.js
+++ bark/src/main-pages/DogList/DogList.js
@@ -26,7 +26,7 @@ function DogList() {
                   title: dog.name,
                   imgUrl: dog.primary_photo_cropped.medium || null,
                   description: {
-                                breed: `${dog.breeds.primary} & ${dog.breeds.secondary}` || dog.breeds.primary,
+                                breed: dog.breeds.secondary === null ? dog.breeds.primary : `${dog.breeds.primary} & ${dog.breeds.secondary}`,
                                 location: `${dog.contact.address.city}  ${dog.contact.address.state}`, 
                                 status: dog.status
                               }
