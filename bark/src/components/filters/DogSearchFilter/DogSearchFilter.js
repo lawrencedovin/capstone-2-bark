b4570487fc1e8246ec241ca2902fc3aaa9d772a5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-11-01 15:39:07 -0400

    added initial error handling for zip code

--- bark/src/components/filters/DogSearchFilter/DogSearchFilter.js
+++ bark/src/components/filters/DogSearchFilter/DogSearchFilter.js
@@ -1,17 +1,39 @@
 import React, {useState} from 'react';
+import UserInput from '../../forms/UserInput/UserInput';
 
 function DogSearchFilter({breeds}) {
+
+  const [breed, setBreed] = useState('All Breeds');
+  const [zipcode, setZipcode] = useState('');
+  let [disabledButton, setDisableButton] = useState(true);
+  let [errorStylesDisplay, setErrorStylesDisplay] = useState(false);
   
   const breedOptions = breeds.map((breed) => {
     return <option className="dropdown-item dog-search-filter__button" value={breed.toLowerCase()}>{breed}</option>
   })
 
-  const [breed, setBreed] = useState('All Breeds');
+  
 
-  const handleChange = e => {
-    // alert(e.target.value)
+  // const handleChange = (e) => {
+  //   const {name, value} = e.target;
+  //   setFormData(formData => ({
+  //     ...formData,
+  //     [name]: value
+  //   }))
+  // }
+
+  const handleBreedChange = e => {
+    alert(e.target.value);
     setBreed(e.target.value);
   };
+  
+
+  const handleZipcodeChange = (e) => {
+    let inputLength = e.target.value.length;
+    inputLength === 5 ? setDisableButton(false) : setDisableButton(true);
+    inputLength > 5 ? setErrorStylesDisplay(true) : setErrorStylesDisplay(false);
+    setZipcode(e.target.value);
+  }
 
   return (
   <div data-testid="DogSearchFilter">
@@ -20,15 +42,21 @@ function DogSearchFilter({breeds}) {
         <form className="dog-search-filter text-center">
           <span className="dog-search-filter__label">Find Best Friend: </span>
           <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <select name="breeds" className="dog-search-filter__button dog-search-filter__button--dropdown" value={breed} onChange={handleChange}>
+            <select name="breeds" className="dog-search-filter__button dog-search-filter__button--dropdown" value={breed} onChange={handleBreedChange}>
               {breedOptions}
             </select>
             <div className="input-group">
-              <input type="text" className="form-control dog-search-filter__input" placeholder="Zip Code" aria-label="Input group example" aria-describedby="btnGroupAddon" />
+              <UserInput 
+                      name="zipcode" 
+                      value={zipcode} 
+                      type="text"
+                      handleChange={handleZipcodeChange}
+                      className="form-control dog-search-filter__input"
+              />
             </div>
-            <a className="btn-primary dog-search-filter__button dog-search-filter__button--icon" href="/search">
+            <button className="btn dog-search-filter__button dog-search-filter__button--icon" href="/search" disabled={disabledButton}>
               <img src={process.env.PUBLIC_URL + 'icons/search-icon.svg'} className="dog-search-filter__icon--search" alt="search icon" />
-            </a>
+            </button>
           </div>
         </form>
       </div>
