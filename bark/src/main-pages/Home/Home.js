67df8415cfac7ff2375b0f5dfc85bb639a0ecc61
Lawrence Dovin <lawrencedovin@gmail.com>
2021-11-05 15:55:33 -0400

    Changes home page dogs to user's signed up zipcode

--- bark/src/main-pages/Home/Home.js
+++ bark/src/main-pages/Home/Home.js
@@ -5,18 +5,19 @@ import BreedsCardsList from '../../components/cards/BreedsCardsList/BreedsCardsL
 import LoadingCardsList from '../../components/cards/LoadingCardsList/LoadingCardsList';
 import DogsCardsList from '../../components/cards/DogsCardsList/DogsCardsList';
 import MainFooter from '../../components/footers/MainFooter/MainFooter';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import { getBreedsData, getDogsData } from '../../helpers/api-helpers';
+import { UserContext } from '../../context/UserContext';
 
 function Home() {
+  const {user, setUser} = useContext(UserContext);
   const [dogs, setDogs] = useState([]);
   const [breeds, setBreeds] = useState([]);
   const [loading, setLoading] = useState(true);
   let getDogs = [];
   let getBreeds = [];
-  let zipcode = 20720;
   let breedsURL = 'https://api.petfinder.com/v2/types/dog/breeds';
-  let dogsURL = `https://api.petfinder.com/v2/animals?location=${zipcode}&type=dog&limit=24`;
+  let dogsURL = user ? `https://api.petfinder.com/v2/animals?location=${user.zipcode}&type=dog&limit=24` : `https://api.petfinder.com/v2/animals?type=dog&limit=24`;
 
   useEffect(() => {
     getBreedsData(breedsURL, getBreeds, setBreeds)
